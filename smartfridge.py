5e22a55660425823bb0882b422720f4529374aa1
steviebald <steviebald@gmail.com>
2017-07-11 22:51:13 +0100

    initial checkin

---
+++ smartfridge.py
@@ -0,0 +1,87 @@
+#To do
+#Figure out how to synch log files
+#cron to check if running and if not restart
+
+from gpiozero import LED
+import time 
+from time import sleep
+import os
+import csv
+
+#load temperature drivers
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+temp_fridge_sensor = "/sys/bus/w1/devices/28-80000002d084/w1_slave"
+temp_external_sensor = "/sys/bus/w1/devices/28-80000002d31a/w1_slave"
+
+FRIDGE_THRESHOLD = 25.0
+LOGGING_FREQUENCY_SECS = 300
+
+relay = LED(14)
+led = LED(26)
+error = False
+
+def getRaw(sensor):
+	try:
+		f = open(sensor, "r")
+		lines = f.readlines()
+		f.close()
+	except:
+		lines = "could find sensor";
+		global error
+		error = True
+	return lines
+
+def getTemperature(sensor):
+	lines = getRaw(sensor)
+	temp_c = -273.0
+	
+	if lines[0].strip()[-3:] != "YES":
+		global error
+		error = True
+		print("error reading temperature sensor " + sensor)
+		logData(temp_c, temp_c, "error reading temperature sensor " + sensor)
+	
+	temp_output = lines[1].find("t=")
+	
+	if temp_output != -1:
+		temp_string = lines[1].strip()[temp_output+2:]
+		temp_c = float(temp_string)/1000.0
+		#print(temp_c)
+	
+	return temp_c
+		
+def logData(fridgeTemp, externalTemp, onOff):
+	strTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+	strDate = time.strftime('%Y%m%d', time.localtime())
+	row = [strTime, fridgeTemp, externalTemp, onOff]
+	with open("/home/pi/Desktop/python3/smartfridge/"+strDate+".csv", "a") as f:
+		w = csv.writer(f)
+		w.writerow(row)
+
+while True:
+
+	error = False
+	
+	tempFridge = getTemperature(temp_fridge_sensor)
+	tempExternal = getTemperature(temp_external_sensor)
+	
+	if error:
+		led.blink(0.1,0.5)
+	else:
+		led.on()
+		print("Fridge temp: " + str(tempFridge))
+		print("External temp: " + str(tempExternal))
+		
+		if tempFridge > FRIDGE_THRESHOLD:
+			relay.off()
+			print("fridge on")
+			logData(str(tempFridge), str(tempExternal), "on")
+		else:
+			relay.on()
+			print("fridge off")
+			logData(str(tempFridge), str(tempExternal), "off")
+			
+	sleep(LOGGING_FREQUENCY_SECS)
+
