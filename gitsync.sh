82ae31b2022bde14d6fc7948354a278e6d6adcd4
steviebald <steviebald@gmail.com>
2017-07-12 18:03:35 +0100

    another go

--- gitsync.sh
+++ gitsync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
 
-current_date_time = date + "%Y-%m-%d %H:%M:%S"
+timestamp=$(date +%s)
 
 cd /home/pi/Desktop/python3/smartfridge/
 git --git-dir /home/pi/Desktop/python3/smartfridge/.git add .
-git --git-dir /home/pi/Desktop/python3/smartfridge/.git commit -m $current_date_time
+git --git-dir /home/pi/Desktop/python3/smartfridge/.git commit -m $timestamp
 git --git-dir /home/pi/Desktop/python3/smartfridge/.git push
 echo "done"
