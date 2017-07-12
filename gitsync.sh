fff471de8358a79edda9beb2f29058cc62d3ed7b
steviebald <steviebald@gmail.com>
2017-07-12 17:55:57 +0100

    added date time to commit message

--- gitsync.sh
+++ gitsync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash  
 
+current_date_time="`date +%Y-%m-%d %H:%M:%S`";
+
 cd /home/pi/Desktop/python3/smartfridge/
 git --git-dir /home/pi/Desktop/python3/smartfridge/.git add .
-git --git-dir /home/pi/Desktop/python3/smartfridge/.git commit -m "new data"
+git --git-dir /home/pi/Desktop/python3/smartfridge/.git commit -m $current_date_time
 git --git-dir /home/pi/Desktop/python3/smartfridge/.git push
 echo "done"
