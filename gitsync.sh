5b766664d874779298852d5b5c26e4d370757028
steviebald <steviebald@gmail.com>
2017-07-12 16:26:15 +0100

    added git-dir so can be run from anywhere

--- gitsync.sh
+++ gitsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
-cd /home/pi/Desktop/python3/smartfridge
-git add .
-git commit -m "new data"
-git push
+
+git --git-dir /home/pi/Desktop/python3/smartfridge/.git add .
+git --git-dir /home/pi/Desktop/python3/smartfridge/.git commit -m "new data"
+git --git-dir /home/pi/Desktop/python3/smartfridge/.git push
 echo "done"
